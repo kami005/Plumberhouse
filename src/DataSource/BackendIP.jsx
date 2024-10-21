79d4bfab90788dc8b90482274f3db29e6d92528e
kami005 <87596308+kami005@users.noreply.github.com>
2024-10-21 06:15:50 +0100

    Update BackendIP.jsx

--- src/DataSource/BackendIP.jsx
+++ src/DataSource/BackendIP.jsx
@@ -1,4 +1,4 @@
 
-export const InternetIP='https://pbhousebackend.herokuapp.com'
+export const InternetIP='https://pbhousebackend.onrender.com'
 const  localIP= 'http://localhost:5000'
 export const ServerIP = `${InternetIP}`
